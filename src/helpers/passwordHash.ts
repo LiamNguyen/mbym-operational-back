e9b46cba6839ed819ee318bae902b3d6c08d0323
Thang <viktor.thang.phan@gmail.com>
2021-03-21 15:14:56 -0400

    Initiated script and create user in db

---
+++ src/helpers/passwordHash.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+
+// export const hashThisPlease =  (password: string) => {
+//   const salt =  bcrypt.genSaltSync(
+//     parseInt(process.env.SALT_ROUNDS as string)
+//   );
+//   console.log(salt, "Salttt")
+//   const res =  bcrypt.hashSync('dfhasdjfsdjhfsdnjkafha', salt);
+//   console.log(res + 'This is from passhelper');
+//   return res;
+// };
+
+const saltRounds = 3;
+export const hashPassword = (plainPassword: string) => {
+  return bcrypt.hashSync(plainPassword, saltRounds);
+};
