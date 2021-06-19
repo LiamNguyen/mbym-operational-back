a893a3050f09cc775c70270634bd3a10af74c9e0
Thang <viktor.thang.phan@gmail.com>
2021-06-19 16:18:56 -0400

    Move api_key and domain name to .env

--- src/createAdminAccountScript.ts
+++ src/createAdminAccountScript.ts
@@ -6,18 +6,20 @@ console.log('Admin entered: ' + process.argv[2]);
 import { DateTime } from 'luxon';
 import mongoose from 'mongoose';
 import generator from 'generate-password';
-var api_key = '6cc2482c7ad83e5fc3208d36cfa2ceb1-24e2ac64-bd60d896';
+
 
 import nodemailer from 'nodemailer';
 import User from './models/User';
 import connectDatabase from './database';
 import { hashPassword } from './helpers/passwordHash';
 var domain = 'sandbox0af18f09df984338921f01bc31e73f76.mailgun.org';
-const mailgun = require('mailgun-js')({
-  apiKey: '6cc2482c7ad83e5fc3208d36cfa2ceb1-24e2ac64-bd60d896',
-  domain: 'sandbox0af18f09df984338921f01bc31e73f76.mailgun.org'
-});
 require('dotenv').config();
+
+const mailgun = require('mailgun-js')({
+  apiKey: process.env.API_KEY,
+  domain: process.env.DOMAIN
+});
+
 connectDatabase();
 
 // // 2. Generate default password
@@ -46,16 +48,24 @@ const result = User.create(userObj).then(data => {
 
 //  Send password to user's email
 // 4. Generate link to reset password UI localhost:7000/password/reset/token
-const data = {
+const registrationLinkEmailData = {
   from: 'Excited User <viktor.thang.phan@gmail.com>',
   to: 'viktor.thang.phan@gmail.com',
   subject: 'Reset for new Password',
-  html: `<p>The random password is ${defaultPassword}</p><p>Click <a href="http://localhost:3000/">here</a> to reset your password</p>`
+  html: `<p>Click <a href="http://localhost:3000/">here</a> to reset your password</p>`
 };
-mailgun.messages().send(data, function (error: any, body: any) {
-  console.log(body);
+const defaultPasswordEmailData = {
+  from: 'Excited User <viktor.thang.phan@gmail.com>',
+  to: 'viktor.thang.phan@gmail.com',
+  subject: 'Reset for new Password',
+  html: `<p>The default password of ${email} is ${defaultPassword}</p>`
+};
+mailgun.messages().send(registrationLinkEmailData, function (error: any, body: any) {
+  console.log(body, 'Registration Link');
 });
-
+mailgun.messages().send(defaultPasswordEmailData, function (error: any, body: any) {
+  console.log(body, 'Default password info');
+});
 // const transporter = nodemailer.createTransport({
 //   service: 'gmail',
 //   auth: {
