a4f015b14bf7139306c7a83066d30aeb05dd29d7
Thang <viktor.thang.phan@gmail.com>
2021-06-20 08:56:23 -0400

    Connects api called object with post routes of user

--- src/routes/reset-password.ts
+++ src/routes/reset-password.ts
@@ -1,42 +1,61 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import { comparePasswords } from '../helpers/passwordHash';
+
+import { comparePasswords, hashPassword } from '../helpers/passwordHash';
 import connect from '../database';
 import User from '../models/User';
+
 const router = express.Router();
+
+//Repositories
 const getUserByEmail = async (email: string) => {
   await connect();
   const response = await User.findOne({ email });
-  console.log(response)
+  console.log(response);
   return response;
 };
 
+const updateDefaultPassword = async (
+  email: string,
+  newpasswordHashed: string
+) => {
+  await connect();
+  const update = await User.findOneAndUpdate(
+    { email },
+    { $set: { passwordHashed: newpasswordHashed } },
+    { new: true }
+  );
+  console.log('Password Updated');
+};
+
+//Routes
 router.route('/reset-password/').post(async (req, res) => {
   const { email, password, newPassword } = req.body;
   if (!email) {
-    res.json({ message: "invalid form submission" });
+    res.json({ message: 'invalid form submission' });
   }
   // Get user with email from DB
   const user = await getUserByEmail(email);
-  console.log(user)
-  res.status(200).send('Found user');
+  console.log(user);
+  // res.status(200).send('Found user');
   //Hash password and compare with in DB
-  const passFromDB = user && user._id ? user.password : null;
+  const passFromDB = user && user._id ? user.passwordHashed : null;
   if (!passFromDB) {
-    return res.json({ status: "Invalid email or password" });
+    console.log(passFromDB, '/n', password);
+    return res.json({ status: 'Invalid email or password' });
   } else {
     const result = await comparePasswords(password, passFromDB);
-    console.log(result);
+    console.log('This is the compare result: ', result);
     if (!result) {
-      return res.json({ status: "Invalid email or password" });
+      console.log('wrong password');
+      return res.json({ status: 'Invalid email or password' });
     } else {
-
-      console.log(user);
-      return res.json({
-        // message: "Log in successfully",
-
-      });
+      console.log('Login in successfully');
+      //Change default password
+      const newpasswordHashed = await hashPassword(newPassword);
+      const response = await updateDefaultPassword(email, newpasswordHashed);
+      console.log(newpasswordHashed);
+      res.json('Thank you');
     }
   }
 });
@@ -46,3 +65,4 @@ router.route('/reset-password/').get((req, res) => {
 });
 export default router;
 //User sign in route
+//0zsjY29oAo
